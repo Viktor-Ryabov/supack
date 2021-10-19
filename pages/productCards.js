7c126b5cb5695984d805efaedbb8831bb5de57d5
Рябов Виктор Александрович <ryabov_va@seligdar.local>
2021-10-19 15:37:21 +0300

    changed foto links

--- pages/productCards.js
+++ pages/productCards.js
@@ -1,26 +1,26 @@
 const productCards = [
   {
     name: "Гречка",
-    link: "",
+    link: "https://sun9-18.userapi.com/impg/QXWuuZVNXHJxWwdBv2BFAtJ206BjZ6xmQS62cg/hi42e4ODY9Q.jpg?size=1079x1080&quality=95&sign=3d8de47fc5e1ebf8dba5c871c91e6d76&type=album",
   },
   {
     name: "Рис",
-    link: "https://wampi.ru/image/RBfrfnE",
+    link: "https://sun9-1.userapi.com/impg/KNQNXGHIjYpZECUZTEf381HkN1PESLrH7504AQ/3ZceaY1GumM.jpg?size=2560x2560&quality=95&sign=84bdb0a90d12a1283c69b900b54696c1&type=album",
   },
   {
     name: "Рис",
-    link: "",
+    link: "https://sun9-85.userapi.com/impg/ZAiirhDHMUny-Et3fQmMwhsJBt0mOpYGF1TTQg/1AXTnnjyfn4.jpg?size=1280x1280&quality=95&sign=e7fb020d45394e4369f0b1d58bd61400&type=album",
   },
   {
     name: "Макароны",
-    link: "",
+    link: "https://sun9-15.userapi.com/impg/ZOrzxSY_jCK0Zh8IKn53BC1RO4tm3ZkjFNlnLQ/ecvasTAiUrc.jpg?size=1280x1280&quality=95&sign=b4e839c92220877d93630c5c60e860d7&type=album",
   },
   {
     name: "Булгур",
-    link: "",
+    link: "https://sun9-13.userapi.com/impg/EqnIMn4jv4_EhJuzjO9nzJMaR3k-ZKZa0Z_QNQ/4IAM1GKZlfM.jpg?size=1280x1280&quality=95&sign=911a6e0a2a461fb4a4058e38d895660f&type=album",
   },
   {
     name: "Греча-завтрак",
-    link: "",
+    link: "https://sun9-72.userapi.com/impg/733qar5vzdVHzfexbrFtqrHmJ53lGGkBoPLGHw/E3t2BKi7y-0.jpg?size=1280x1208&quality=95&sign=ca6e7b1f26b78da25707b892220af3dd&type=album",
   },
 ];
